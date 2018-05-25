32d5b8fdc4137907c0866e0fed3633837bff8f81
Ivan <ivan.zderadicka@gmail.com>
2018-05-25 20:56:00 +0200

    Another rather simplistic implementation - using blocking function to wrap blocking file I/O

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,70 @@
+extern crate futures;
 extern crate rand;
 extern crate tokio;
 extern crate tokio_threadpool;
 
+use futures::future::poll_fn;
 use rand::Rng;
 use std::env;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::path::{Path, PathBuf};
+use std::sync::Arc;
 use std::time::Duration;
 use tokio::io;
 use tokio::net::TcpListener;
 use tokio::prelude::*;
+use tokio_threadpool::blocking;
 
 fn prepare_server(
     addr: std::net::SocketAddr,
     file_name: PathBuf,
-    idx: Index,
+    idx: Arc<Index>,
 ) -> Box<Future<Item = (), Error = io::Error> + Send> {
     println!("Starting at {}", &addr);
     let tcp = TcpListener::bind(&addr).unwrap();
 
     let server = tcp.incoming().for_each(move |socket| {
         println!("Received connection from {}", socket.peer_addr().unwrap());
-        
-        let i = rand::thread_rng().gen_range(0, idx.len());
-        let (from, to) = idx[i];
-        println!("Sending joke from lines: {} - {}", from, to);
-        let reader = BufReader::new(File::open(&file_name).unwrap());
-        let joke: Vec<_> = reader
-            .lines()
-            .skip(from)
-            .take(to - from)
-            .filter(|r| r.is_ok())
-            .map(|s| s.unwrap())
-            .filter_map(|l| {
-                let s = l.trim_left();
-                if s.len() > 1 {
-                    Some(s.to_owned())
-                   
-                } else {
-                None
-                }
-            })
-            .collect();
+        let file_name = file_name.clone();
+        let idx = idx.clone();
+        let work_future = poll_fn(move || {
+            blocking(|| {
+                let i = rand::thread_rng().gen_range(0, idx.len());
+                let (from, to) = idx[i];
+                println!("Sending joke from lines: {} - {}", from, to);
+                let reader = BufReader::new(File::open(&file_name).unwrap());
+                let joke: Vec<_> = reader
+                    .lines()
+                    .skip(from)
+                    .take(to - from)
+                    .filter(|r| r.is_ok())
+                    .map(|s| s.unwrap())
+                    .filter_map(|l| {
+                        let s = l.trim_left();
+                        if s.len() > 1 {
+                            Some(s.to_owned())
+                        } else {
+                            None
+                        }
+                    })
+                    .collect();
 
-        let mut text = joke.join("\n");
-        text.push_str("\n");
-        let write_future= io::write_all(socket, text)
-        .then(|res| {
-            println!("Written joke -result is Ok {:?}",res.is_ok());
-            Ok(())
+                let mut text = joke.join("\n");
+                text.push_str("\n");
+                text
+            })
         });
+        let write_future = work_future
+            .map_err(|_| std::io::Error::new(std::io::ErrorKind::Other, "Blocking Error"))
+            .and_then(|text| {
+                //println!("Joke is {}", text);
+                io::write_all(socket, text)
+            })
+            .then(|res| {
+                println!("Written joke -result is Ok {:?}", res.is_ok());
+                Ok(())
+            });
 
         tokio::spawn(write_future);
 
@@ -105,7 +119,7 @@ fn main() {
         }
     };
     let idx = create_index(&jokes_file).unwrap();
-    //let idx = Arc::new(idx);
+    let idx = Arc::new(idx);
 
     let addr = "127.0.0.1:12345".parse().unwrap();
     let server = prepare_server(addr, jokes_file.into(), idx);
