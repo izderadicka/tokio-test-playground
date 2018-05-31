8e6383d67b311f47848955812bd305bf4e349b4c
Ivan <ivan.zderadicka@gmail.com>
2018-05-31 14:52:48 +0200

    fix + formating

--- src/main.rs
+++ src/main.rs
@@ -5,50 +5,43 @@ extern crate tokio_threadpool;
 extern crate futures;
 extern crate tokio_io;
 
+use futures::sink::Sink;
+use futures::stream::iter_ok;
 use rand::Rng;
 use std::env;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
-use std::path::{Path};
+use std::path::Path;
 use std::time::Duration;
 use tokio::io;
 use tokio::net::{TcpListener, TcpStream};
 use tokio::prelude::*;
-use futures::stream::iter_ok;
-use futures::sink::{Sink};
 
 static mut GLOBAL_INDEX: Option<Index> = None;
 
 fn get_index() -> &'static Index {
-    unsafe {
-        GLOBAL_INDEX.as_ref().unwrap()
-    }
+    unsafe { GLOBAL_INDEX.as_ref().unwrap() }
 }
 
-fn prepare_server(
-    addr: std::net::SocketAddr
-) -> Box<Future<Item = (), Error = io::Error> + Send> {
+fn prepare_server(addr: std::net::SocketAddr) -> Box<Future<Item = (), Error = io::Error> + Send> {
     println!("Starting at {}", &addr);
     let tcp = TcpListener::bind(&addr).unwrap();
 
     let server = tcp.incoming().for_each(move |socket| {
         let idx = get_index();
-        println!("Received connection from {}", socket.peer_addr().unwrap());
+        //println!("Received connection from {}", socket.peer_addr().unwrap());
         let i = rand::thread_rng().gen_range(0, idx.index.len());
         let (from, to) = idx.index[i];
-        println!("Sending joke from lines: {} - {}", from, to);
-        
-        let joke_iter= idx
-            .lines.iter()
-            .skip(from)
-            .take(to - from)
-            ;
+        //println!("Sending joke from lines: {} - {}", from, to);
+
+        let joke_iter = idx.lines.iter().skip(from).take(to - from);
 
         let stream = iter_ok(joke_iter);
         let sink = LineSink::new(socket);
-        let write_future= stream.forward(sink)
-        .map_err(|e:io::Error| eprintln!("Write error: {}", e))
-        .map(|_| ());
+        let write_future = stream
+            .forward(sink)
+            .map_err(|e: io::Error| eprintln!("Write error: {}", e))
+            .map(|_| ());
 
         tokio::spawn(write_future);
 
@@ -63,18 +56,17 @@ struct LineSink {
     line: Option<&'static [u8]>,
     line_pos: usize,
     next_line: usize,
-    lines: Vec<&'static [u8]>
+    lines: Vec<&'static [u8]>,
 }
 
 impl LineSink {
     fn new(socket: TcpStream) -> Self {
         LineSink {
-            socket:socket,
+            socket: socket,
             line: None,
             line_pos: 0,
             next_line: 0,
-            lines: Vec::with_capacity(20)
-
+            lines: Vec::with_capacity(20),
         }
     }
 }
@@ -82,42 +74,41 @@ impl LineSink {
 impl Sink for LineSink {
     type SinkItem = &'static String;
     type SinkError = io::Error;
-    fn start_send(&mut self, item: Self::SinkItem) -> Result<AsyncSink<Self::SinkItem>, Self::SinkError> {
-        println!("SINK - start send line: {}", item);
+    fn start_send(
+        &mut self,
+        item: Self::SinkItem,
+    ) -> Result<AsyncSink<Self::SinkItem>, Self::SinkError> {
         self.lines.push(item.as_bytes());
         self.line_pos = 0;
         Ok(AsyncSink::Ready)
     }
 
     fn poll_complete(&mut self) -> Result<Async<()>, Self::SinkError> {
-         println!("SINK - calling poll_complete with line_num = {}/{}", self.next_line, self.lines.len());
         while self.next_line <= self.lines.len() {
             match self.line {
                 Some(line) => {
                     while self.line_pos < line.len() {
-                        let n = try_ready!( self.socket.poll_write(&line[self.line_pos..]));
-                        println!("send {} bytes of line {}", n, std::str::from_utf8(line).unwrap());
+                        let n = try_ready!(self.socket.poll_write(&line[self.line_pos..]));
                         if n == 0 {
                             return Err(io::Error::new(io::ErrorKind::Other, "write 0 bytes"));
-                            }
+                        }
                         self.line_pos += n
                     }
                     let _n = try_ready!(self.socket.poll_write(b"\n"));
                     self.line = None;
                     self.line_pos = 0
-                },
+                }
                 None => {
-                    self.line = Some(self.lines[self.next_line]);
-                     println!("Moved to line {}", self.next_line);
-                    self.next_line+=1;
+                    if self.next_line < self.lines.len() {
+                        self.line = Some(self.lines[self.next_line]);
+                    }
+                    self.next_line += 1;
                 }
             }
         }
         Ok(Async::Ready(()))
     }
 }
-
-
 
 fn create_runtime() -> Result<tokio::runtime::Runtime, io::Error> {
     let mut tp_builder = tokio_threadpool::Builder::new();
@@ -133,7 +124,7 @@ fn create_runtime() -> Result<tokio::runtime::Runtime, io::Error> {
 
 struct Index {
     index: Vec<(usize, usize)>,
-    lines: Vec<String>
+    lines: Vec<String>,
 }
 
 fn create_index<P: AsRef<Path>>(f: P) -> Result<(), std::io::Error> {
@@ -155,10 +146,9 @@ fn create_index<P: AsRef<Path>>(f: P) -> Result<(), std::io::Error> {
                     let s = l.trim_left();
                     if s.len() > 1 {
                         lines.push(s.to_owned());
-                        no+=1;
+                        no += 1;
                     }
                 }
-                
             }
 
             Err(e) => eprintln!("Error reading line {}: {}", no, e),
@@ -167,10 +157,7 @@ fn create_index<P: AsRef<Path>>(f: P) -> Result<(), std::io::Error> {
     // It's ok to use unsafe, as initialization happens in main thread before any other thread starts
     // later threads only read from it
     unsafe {
-    GLOBAL_INDEX = Some(Index {
-        index:idx,
-        lines
-    });
+        GLOBAL_INDEX = Some(Index { index: idx, lines });
     }
     Ok(())
 }
